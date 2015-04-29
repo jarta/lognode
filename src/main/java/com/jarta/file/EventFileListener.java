b4b2dff1174309243fe7cd77a4371f3bc551f9cd
jarta <dongw1980@hotmail.com>
2015-04-30 00:36:26 +0800

    init version

--- src/main/java/com/jarta/file/EventFileListener.java
+++ src/main/java/com/jarta/file/EventFileListener.java
@@ -8,26 +8,43 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
  * Created by wei on 2015/4/4.
  */
-public class EventFileListener {
+public class EventFileListener implements Runnable{
 
     private static Logger logger = LoggerFactory.getLogger(EventFileListener.class);
+
+    private final int TIMEOUT = 5*1000;
 
     private static Map<String, EventFileListener> watchRegistry = new HashMap<String, EventFileListener>();
 
     private Map<String,LogFileStat> fileRegistry = new HashMap<String, LogFileStat>();
 
+    private Map<String,Long> fileInitSize = new HashMap<String, Long>();
+
     private String filePath;
 
     private WatchService watcher;
 
-    private EventFileListener(String filePath){
-        this.filePath = filePath;
+    private boolean runFlag = true;
 
+    private EventFileListener(String filePath) throws Exception {
+        this.filePath = filePath;
+        //record the init file size
+        File fDir = new File(filePath);
+        if(!fDir.isDirectory()) {
+            logger.error("target location is not a valid folder - {}", filePath);
+            throw new Exception(String.format("Invalid monitoring path - %s", filePath));
+        }
+
+        File[] files = fDir.listFiles();
+        for(int i=0;i<files.length;i++){
+            fileInitSize.put(files[i].getName(), files[i].length());
+        }
     }
 
     /**
@@ -62,9 +79,10 @@ public class EventFileListener {
                 for(WatchEvent<?> watchEvent: wKey.pollEvents()) {
 
                     final WatchEvent<Path> watchEventPath = (WatchEvent<Path>) watchEvent;
-                    final Path file = watchEventPath.context();
+                    final Path filename = watchEventPath.context();
+                    File file = new File(this.filePath + File.separator + filename.toFile().getName());
 
-                    logger.info("event coming for " + file.toFile().getName() + " , event:" + watchEvent.kind().name());
+                    logger.info("event coming for " + file.getPath() + " , event:" + watchEvent.kind().name());
                     LogFileStat curStat = getLogFileStat(file);
 
                     if(StandardWatchEventKinds.ENTRY_CREATE == watchEvent.kind()) {
@@ -72,13 +90,12 @@ public class EventFileListener {
                     }
 
                     if(StandardWatchEventKinds.ENTRY_MODIFY == watchEvent.kind()) {
-                        curStat.modify();
+                        curStat.modify(false);
                     }
 
                     if(StandardWatchEventKinds.ENTRY_DELETE == watchEvent.kind()) {
                         curStat.remove();
                     }
-
                 }
             }
 
@@ -96,17 +113,41 @@ public class EventFileListener {
      * @param file
      * @return
      */
-    private LogFileStat getLogFileStat(Path file) {
+    private LogFileStat getLogFileStat(File file) {
         LogFileStat curStat = null;
-        String chgFile = file.toFile().getName();
+        String chgFile =file.getName();
         synchronized (fileRegistry) {
             if(!fileRegistry.containsKey(chgFile)) {
                 //TODO: the default position should be read when process up
-                fileRegistry.put(chgFile, new LogFileStat(file, 0));
+
+                fileRegistry.put(chgFile, new LogFileStat(file, toLastLine(file)));
             }
         }
         curStat = fileRegistry.get(chgFile);
         return curStat;
+    }
+
+    /**
+     * Remove the stale file
+     * @param fileName
+     */
+    private void removeLogFileStat(String fileName) {
+        synchronized (fileRegistry) {
+            fileRegistry.remove(fileName);
+        }
+    }
+
+    /**
+     *
+     * @param file
+     * @return
+     */
+    private long toLastLine(File file) {
+        Long initSize = fileInitSize.remove(file.getName());
+        if(initSize!=null && file.length() > initSize) {
+            return initSize;
+        }
+        return 0;
     }
 
 
@@ -114,23 +155,42 @@ public class EventFileListener {
      * initiate new file watcher for the given file path
      * @param filePath
      */
-    public static void addWatch(String filePath) {
+    public static void addWatch(String filePath) throws Exception {
         File target = new File(filePath);
         if(target.exists() && target.isDirectory()) {
             synchronized (watchRegistry) {
                 if(!watchRegistry.containsKey(filePath)) {
                     final EventFileListener fileWatcher = new EventFileListener(filePath);
-                    fileWatcher.startWatch();
-
                     watchRegistry.put(filePath, fileWatcher);
+                    new Thread(fileWatcher).start();
                 }
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    @Override
+    public void run() {
+        startWatch();
+        while(runFlag) {
+            synchronized (fileRegistry) {
+                Iterator<LogFileStat> fileIter = fileRegistry.values().iterator();
+                while(fileIter.hasNext()) {
+                    LogFileStat f = fileIter.next();
+                    if(f.isNeedFlash() && f.isOverTime(TIMEOUT)) {
+                        f.modify(true);
+                    }
+                }
+            }
 
-
+            try {
+                Thread.sleep(30 * 1000);
+            } catch (InterruptedException e) {
+            }
+        }
     }
 
+    public static void main(String[] args) throws Exception {
+        EventFileListener.addWatch("D:\\works");
+        Thread.currentThread().join();
+    }
 }
--- src/main/java/com/jarta/file/LogFileStat.java
+++ src/main/java/com/jarta/file/LogFileStat.java
@@ -5,10 +5,13 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.InputStream;
 import java.nio.file.Path;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by wei on 2015/4/5.
@@ -17,13 +20,12 @@ public class LogFileStat {
 
     private Logger logger = LoggerFactory.getLogger(LogFileStat.class);
 
-    private Path logFile;
+    private File logFile;
     private long position;
 
-    /** unfinished line or exception stack */
-    private String dirtyBuffer;
+    private AtomicBoolean needFlash = new AtomicBoolean(false);
 
-    public LogFileStat(Path f, long position) {
+    public LogFileStat(File f, long position) {
         logFile = f;
         this.position = position;
     }
@@ -35,15 +37,30 @@ public class LogFileStat {
         }
     }
 
-    public void modify() {
+    /**
+     * File is updated
+     * @param forceRefresh - whether force refresh
+     * @return if any dirty line needs to be processed later.
+     */
+    public void modify(boolean forceRefresh) {
         InputStream is = null;
         try {
-            is = FileUtils.openInputStream(logFile.toFile());
+            is = FileUtils.openInputStream(logFile);
             IOUtils.skip(is, position);
             List<String> lines = IOUtils.readLines(is);
-            updatePosition(lines);
 
-            dirtyBuffer = lines.remove(lines.size());
+            //ignore the last line for un-finished consideration
+            if(!forceRefresh && lines.size() > 0) {
+                lines.remove(lines.size() - 1);
+                needFlash.set(true);
+            }
+
+            //force refresh reset to default
+            if(forceRefresh) {
+                needFlash.set(false);
+            }
+
+            updatePosition(lines);
             processRecords(lines);
         } catch (Exception e) {
             logger.error("fail to continue ..", e);
@@ -52,8 +69,22 @@ public class LogFileStat {
         }
     }
 
-    private void processRecords(List<String> lines) {
+    /**
+     * Whether to be flash in time
+     * @return
+     */
+    public boolean isNeedFlash() {
+        return needFlash.get();
+    }
 
+    /**
+     * TODO - publish to redis
+     * @param lines
+     */
+    void processRecords(List<String> lines) {
+        for(String line: lines) {
+            logger.info("todo - {}", line );
+        }
     }
 
     /**
@@ -65,20 +96,26 @@ public class LogFileStat {
         for(String line: lines) {
             bufSize += line.length() + IOUtils.LINE_SEPARATOR.length();
         }
-        bufSize -= IOUtils.LINE_SEPARATOR.length();
-        this.position = bufSize;
+        this.position += bufSize;
     }
 
     /**
      * clean up the buffer
      */
     public void remove() {
-        modify();
-        List<String> tmpList = Collections.emptyList();
-        tmpList.add(dirtyBuffer);
-        processRecords(tmpList);
-
+        //flash
+        modify(true);
         //reset position
         position = 0;
+    }
+
+    /**
+     * Check if the file has been changed for while
+     * @param timeout
+     * @return
+     */
+    public boolean isOverTime(long timeout) {
+        long pastTime = Calendar.getInstance().getTimeInMillis() - logFile.lastModified();
+        return pastTime > timeout;
     }
 }
